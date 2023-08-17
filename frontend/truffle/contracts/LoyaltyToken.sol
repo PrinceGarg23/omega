a717043d4c81a013eb972bba3d1652a861043bc7
Prince Garg <43511492+PrinceGarg23@users.noreply.github.com>
2023-08-17 18:13:04 +0000

    sc

--- frontend/truffle/contracts/LoyaltyToken.sol
+++ frontend/truffle/contracts/LoyaltyToken.sol
@@ -5,6 +5,11 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract LoyaltyToken is ERC20 {
     address private owner;
+    mapping(address => bool) private sellers;
+    mapping(address => uint256) private lastActionTimestamp;
+    mapping(address => uint256) private redeemableTokens;
+
+    uint256 public tokenDecayPeriod = 1 weeks; // Decay period is set to 1 week
 
     constructor(
         string memory _name,
@@ -18,7 +23,67 @@ contract LoyaltyToken is ERC20 {
         _;
     }
 
+    modifier onlySeller() {
+        require(
+            sellers[msg.sender],
+            "Only authorized sellers can call this function"
+        );
+        _;
+    }
+
+    function addSeller(address seller) public onlyOwner {
+        sellers[seller] = true;
+    }
+
+    function removeSeller(address seller) public onlyOwner {
+        sellers[seller] = false;
+    }
+
     function mintToPool(uint256 amount) public onlyOwner {
         _mint(address(this), amount);
+    }
+
+    function setTokenDecayPeriod(uint256 period) public onlyOwner {
+        tokenDecayPeriod = period;
+    }
+
+    function issueTokens(address buyer, uint256 amount) public onlySeller {
+        require(
+            balanceOf(address(this)) >= amount,
+            "Insufficient tokens in the pool"
+        );
+        _transfer(address(this), buyer, amount);
+        lastActionTimestamp[buyer] = block.timestamp;
+        redeemableTokens[buyer] += amount;
+    }
+
+    function updateTokens(address account) internal {
+        uint256 elapsedTime = block.timestamp - lastActionTimestamp[account];
+        uint256 decayedTokens = (balanceOf(account) * elapsedTime) /
+            tokenDecayPeriod;
+        redeemableTokens[account] =
+            redeemableTokens[account] +
+            balanceOf(account) -
+            decayedTokens;
+        lastActionTimestamp[account] = block.timestamp;
+    }
+
+    function getEarnedTokens(address buyer) public returns (uint256) {
+        updateTokens(buyer); // Update tokens before calculation
+        return redeemableTokens[buyer];
+    }
+
+    function getRedeemableTokens(address buyer) public view returns (uint256) {
+        return redeemableTokens[buyer];
+    }
+
+    function redeemTokens(uint256 amount) public {
+        require(
+            redeemableTokens[msg.sender] >= amount,
+            "Not enough redeemable tokens"
+        );
+        updateTokens(msg.sender); // Update tokens before redeeming
+        redeemableTokens[msg.sender] -= amount;
+        _burn(msg.sender, amount);
     }
 }
